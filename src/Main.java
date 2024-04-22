93316d26602810f1cf7f5967cf0d81a5bdfcd699
wondo8449 <wondo8448@gmail.com>
2024-04-22 20:19:28 +0900

    사직연산 입력 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         // 입력 받을 두 정수를 담을 int 타입의 변수 생성 및 초기화
         int firstNumber, secondNumber = 0;
+        // 입력받을 사칙연산 기호를 담을 변수 생성 및 초기화
+        char symbol = ' ';
+
 
         // 첫번째 숫자를 입력받기 위한 문장 출력
         System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -18,5 +21,13 @@ public class Main {
 
         // 숫자를 정상적으로 입력받는지 테스트하기 위한 출력 메소드
         System.out.println("첫번째 숫자: " + firstNumber + " 두번째 숫자: " + secondNumber);
+
+        // 사칙연산 기호를 입력받기 위한 문장 출력
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 입력받은 문자열(String)을 선언해놓은 변수인 char 타입으로 형변화 후 변수에 담아준다
+        symbol = sc.next().charAt(0);
+
+        // 기호를 정상적으로 입력받는지 테스트하기 위한 출력 메소드
+        System.out.println("입력받은 문자 : " + symbol);
     }
 }
